474accab76c5fa6b6ab48a9c995a8751f5fedb3f
Lucas Alves <45833066+kimbres@users.noreply.github.com>
2019-11-13 20:35:44 -0300

    cadastro usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,9 @@ const routes: Routes = [
   { path: 'perfil-usuario', loadChildren: './pages/perfil-usuario/perfil-usuario.module#PerfilUsuarioPageModule' },
   { path: 'perfil-produto', loadChildren: './pages/perfil-produto/perfil-produto.module#PerfilProdutoPageModule' },
   { path: 'list-produto', loadChildren: './pages/list-produto/list-produto.module#ListProdutoPageModule' },
-  { path: 'endereco', loadChildren: './model/endereco/endereco.module#EnderecoPageModule' }
+  { path: 'endereco', loadChildren: './model/endereco/endereco.module#EnderecoPageModule' },
+  { path: 'add-usuario', loadChildren: './pages/add-usuario/add-usuario.module#AddUsuarioPageModule' },
+  { path: 'add-produto', loadChildren: './pages/add-produto/add-produto.module#AddProdutoPageModule' }
 ];
 
 @NgModule({
