77d88959025452ab21665d14452559833bbd2af8
Joca silva <45833023+maiconluizdemelobrandao@users.noreply.github.com>
2019-11-18 21:33:12 -0300

    cadastro de usuario completo

--- src/app/pages/add-usuario/add-usuario.page.ts
+++ src/app/pages/add-usuario/add-usuario.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/model/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { MensagemService } from 'src/app/services/mensagem.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-usuario',
@@ -10,13 +13,31 @@ export class AddUsuarioPage implements OnInit {
 
   protected usuario: Usuario = new Usuario;
 
-  constructor() { }
+  constructor(
+    private usuarioService:UsuarioService,
+    private msg:MensagemService,
+    private router:Router
+
+  ) { }
 
   ngOnInit() {
   }
 
-  onSubmit (){
+  onSubmit (form){ 
     console.log(this.usuario);
+    this.usuarioService.add(this.usuario).then(
+      res=>{
+        //console.log("Cadastrado!", res);
+        this.msg.presentAlert("Aviso","Cadastrado com Sucesso!");
+        this.usuario = new Usuario;
+        form.reset();
+        this.router.navigate(['']);
+      }, 
+      erro=>{
+        console.log("Erro: ", erro);
+        this.msg.presentAlert("Ops!","Erro ao tentar cadastrar.Verifique se o email já foi cadastrado!");
+      }
+    )
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(
+    private firedb: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) { }
+
+  add(usuario:Usuario) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(usuario.email,usuario.pws).then(
+      res=>{
+        usuario.pws= null;
+        usuario.email = null;
+        return this.firedb.object("usuarios/"+res.user.uid).set(usuario).then().catch(
+          ()=>{
+            this.afAuth.auth.currentUser.delete();
+          });
+        // (usuario) ({
+        //   nome: usuario.nome,
+        //   ativo: usuario.ativo,
+        // })
+      },
+      erro=>{
+        this.afAuth.auth.currentUser.delete(); // para deleter autenticação em caso de erro. 
+      }
+    )
+   // return this.firedb.object("usuarios").set(usuario);
+   // return this.firedb.list("usuarios").push(usuario);
+    }
+
+  get(){
+    let user = this.afAuth.auth.currentUser;
+    console.log(user);
+    return this.firedb.object("usuarios/"+user.uid).valueChanges();
+  }
+
+  update(usuario:Usuario){
+    let user = this.afAuth.auth.currentUser;
+    return this.firedb.object("usuarios/"+user.uid).update(usuario);
+  }
+
+  delete(usuario:Usuario){
+    let uid = this.afAuth.auth.currentUser.uid;
+    this.afAuth.auth.currentUser.delete();
+    return this.firedb.object("usuarios/" + uid).update({ativo:false});
+  }
 }
