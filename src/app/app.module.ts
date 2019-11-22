f9e309239daecb9c2f1df5bd8d95b993f53cc76c
Lucas Alves <45833066+kimbres@users.noreply.github.com>
2019-11-22 19:24:20 -0300

    adicionado funçoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import{ AngularFireDatabaseModule } from "@angular/fire/database"
+import { AppComponent } from './app.component';
 
+//Firebase -------------------------
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabaseModule } from "@angular/fire/database"
+import { AngularFireAuthModule } from '@angular/fire/auth';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [
     StatusBar,
@@ -28,4 +29,4 @@ import{ AngularFireDatabaseModule } from "@angular/fire/database"
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/add-usuario/add-usuario.page.ts
+++ src/app/pages/add-usuario/add-usuario.page.ts
@@ -1,7 +1,9 @@
-import { Usuario } from './../../model/usuario';
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/model/usuario';
-
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { MensagemService } from 'src/app/services/mensagem.service';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-add-usuario',
@@ -10,19 +12,50 @@ import { Usuario } from 'src/app/model/usuario';
 })
 export class AddUsuarioPage implements OnInit {
 
-
   protected usuario: Usuario = new Usuario;
 
-
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private msg: MensagemService,
+    private router:Router,
+    private camera: Camera
+  ) { }
 
   ngOnInit() {
   }
 
-
-  onSubmit (){
-    console.log(this.usuario);
-
+  onSubmit(form) {
+    //console.log(this.usuario);
+    this.usuarioService.add(this.usuario).then(
+      res => {
+        //console.log("Cadastrado! ", res);
+        this.msg.presentAlert("OK, ok!", "Cadastrado com sucesso!");
+        this.usuario = new Usuario;
+        form.reset();
+        this.router.navigate(['']);
+      },
+      erro => {
+        console.log("Erro: ", erro);
+        this.msg.presentAlert("Ops!", "Erro ao tentar cadastrar!\nVerique os dados ou se o e-mail já foi cadastrado!" );
+      }
+    )
   }
 
+
+  tirarFoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
 }
